3ea84c5f01df833eba3483212f12f41b9794b204
Ivaylo Georgiev <120925473+Ivaylo2201@users.noreply.github.com>
2025-05-24 18:24:12 +0300

    Backend setup - Installed dependencies, created core directories

---
+++ Backend/Database/DatabaseContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Database;
+
+public class DatabaseContext(DbContextOptions<DatabaseContext> options) : DbContext(options)
+{
+    // public DbSet<Product> Products => Set<Product>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // modelBuilder.ApplyConfiguration(new ProductConfiguration());
+        
+        base.OnModelCreating(modelBuilder);
+    }
+}
---
+++ Backend/Program.cs
@@ -0,0 +1,72 @@
+using System.Security.Claims;
+using System.Text;
+using Backend.Database;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Scalar.AspNetCore;
+using DotNetEnv;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+Env.Load();
+
+var jwtSecretKey = Encoding.UTF8.GetBytes(Environment.GetEnvironmentVariable("JWT_SECRET_KEY")!);
+var jwtIssuer = Environment.GetEnvironmentVariable("JWT_ISSUER");
+var jwtAudience = Environment.GetEnvironmentVariable("JWT_AUDIENCE");
+var connectionString = Environment.GetEnvironmentVariable("DEFAULT_CONNECTION");
+
+builder.Services.AddControllers();
+builder.Services.AddOpenApi();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            NameClaimType = ClaimTypes.NameIdentifier,
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = jwtIssuer,
+            ValidAudience = jwtAudience,
+            IssuerSigningKey = new SymmetricSecurityKey(jwtSecretKey)
+        };
+    });
+
+builder.Services.AddAuthorization();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
+
+builder.Services.AddDbContext<DatabaseContext>(options => 
+    options.UseSqlServer(connectionString));
+
+// builder.Services.AddSingleton<TokenService>();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();            
+    app.MapScalarApiReference();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+app.UseCors("AllowAll");
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseStaticFiles();
+
+app.MapControllers();
+
+app.Run();
