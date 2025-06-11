ad694544ee374355b50216de25bf3248b774943b
Ivaylo2201 <ivailo_g03@abv.bg>
2025-06-11 16:51:24 +0300

    Removed some unnecessary queries, frontend project setup - installed dependencies

--- backend/Application/CQRS/Reviews/Queries/ReadReviewQuery.cs
+++
@@ -1,7 +0,0 @@
-﻿using Application.DTOs.Review;
-using Core.Abstractions;
-using MediatR;
-
-namespace Application.CQRS.Reviews.Queries;
-
-public record ReadReviewsQuery(Guid ProductId) : IRequest<Result<List<GetReviewDto>?>>;
